2ffc93bae4ab2569c6d05c005312947844d419f6
yuanyu90221 <yuanyu90221@gmail.com>
2016-01-13 12:36:24 +0800

    refine Last

---
+++ FiveChess/src/demo/yuanyu/model/SelfResources.java
@@ -0,0 +1,11 @@
+package demo.yuanyu.model;
+
+public class SelfResources {
+	public static final String RESTART = "重新開始";
+	public static final String EXIT = "退出";
+	public static final String REGRADE = "悔棋";
+	public static final String GAME_NAME = "單機五子棋"; 
+	public static final String SYSTEM = "系統";
+	public static final String BLACK_CHESS = "黑棋", WHITE_CHESS="白棋";
+	public static final String WINNER_MSG = "恭喜，%s贏了!";
+}
